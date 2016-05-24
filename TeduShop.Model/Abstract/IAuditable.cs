3808297d0d63e9dd5f09503bbecb69c9501ef664
Luong Tran <ltran@2clicksolutions.onmicrosoft.com>
2016-05-25 00:16:01 +0700

    add domain entities for project

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreateDate { get; set; }
+        string CreateBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
---
+++ TeduShop.Model/Models/Product.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Products")]
+    public class Product : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        [Column(TypeName = "varchar")]
+        public string Alias { get; set; }
+
+        [Required]
+        public int CategoryID { get; set; }
+
+        [MaxLength(256)]
+        public string Image { get; set; }
+
+        public XElement MoreImages { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal? PromotionPrice { get; set; }
+
+        public int Warranty { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+
+        public bool? HomeFlg { get; set; }
+        public bool? HotFlg { get; set; }
+        public int? ViewCount { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual PropductCategory ProductCategory { get; set; }
+
+        //public virtual IEnumerable<OrderDetail> OrderDetails { get; set; }
+    }
+}
