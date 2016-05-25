edf0ec02d590c8145062d404c0ee947a9cbf6420
Luong Tran <ltran@2clicksolutions.onmicrosoft.com>
2016-05-25 23:44:36 +0700

    update repository class

---
+++ TeduShop.Data/Repositories/FooterRepository.cs
@@ -0,0 +1,17 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IFooterRepository
+    {
+
+    }
+
+    public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
+    {
+        public FooterRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
