0e4512f855fbd8dfba79744005213b8f82bbe664
Luong Tran <ltran@2clicksolutions.onmicrosoft.com>
2016-07-10 17:20:49 +0700

    bai19

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -2,16 +2,28 @@
 
 var myApp = angular.module('myModule', []);
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service('Validator', Validator);
 
-//declare
-function schoolController($scope) {
-    $scope.message = "This is my message from school controller";
+schoolController.$inject = ['$scope', 'Validator'];
+
+// Controller
+function schoolController($scope, Validator) {
+    $scope.num = 1;
+    $scope.onCheckNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
 }
-function studentController($scope) {
-    $scope.message = "This is my message from student controller";
-}
-function teacherController($scope) {
-    //$scope.message = "This is my message from teacher controller";
+
+
+// Service
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return "This is even";
+        }
+        return 'This is odd';
+    }
 }
