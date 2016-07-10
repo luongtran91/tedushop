dda8aef6f65a94d998ccb2ddc5af208f59165edd
Luong Tran <ltran@2clicksolutions.onmicrosoft.com>
2016-07-10 16:59:35 +0700

    update bai18

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+//declare
+function schoolController($scope) {
+    $scope.message = "This is my message from school controller";
+}
+function studentController($scope) {
+    $scope.message = "This is my message from student controller";
+}
+function teacherController($scope) {
+    //$scope.message = "This is my message from teacher controller";
+}
